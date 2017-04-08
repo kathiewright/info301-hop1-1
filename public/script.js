56042a92544bc6c75c73302a5814b8d4895ca478
System <none>
2017-04-08 00:39:29 +0000

    💓🛩 Updated with Glitch

---
+++ public/script.js
@@ -0,0 +1,13 @@
+/*
+  Project:   Hands-On Project 1-1
+	Author:    Kathie Wright
+	Date:      February 2, 2016
+	Purpose:   To demonstrate the basic structure of a web project.
+*/
+
+document.write("<ol>");
+document.write("<li>Reduce spending on non-necessities.</li>");
+document.write("<li>Use extra money to pay off debt, starting with highest-interest credit card.</li>");
+document.write("<li>Continue paying off debts until you are debt free.</li>");
+document.write("<li>Put a fixed percent of your pay aside in savings every payday.</li>");            
+document.write("</ol>"); 
